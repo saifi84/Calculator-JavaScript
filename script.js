69b9b452be9cbac26157721e547372b3552dc6d2
Saif Ul Rehman <63366917+saifi84@users.noreply.github.com>
2022-11-15 15:37:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+
+const res = document.getElementById("result");
+// const toast = document.getElementById("toast");
+
+function cal(value) {
+  const calculatedValue = eval(value || null);
+  if (isNaN(calculatedValue)) {
+    res.value = "Math Error";
+    // setTimeout(() => {
+    //   res.value = "";
+    // }, 1300);
+  } else {
+    res.value = calculatedValue;
+  }
+}
+
+
+function Result(inputValue) {
+  if (!res.value) {
+    res.value = "";
+  }
+  res.value += inputValue;
+}
+
+
+document.addEventListener("keydown", keyboardInputHandler);
+
+
+function keyboardInputHandler(e) {
+
+  e.preventDefault();
+
+  if (e.key === "0") {
+    res.value += "0";
+  } else if (e.key === "1") {
+    res.value += "1";
+  } else if (e.key === "2") {
+    res.value += "2";
+  } else if (e.key === "3") {
+    res.value += "3";
+  } else if (e.key === "4") {
+    res.value += "4";
+  } else if (e.key === "5") {
+    res.value += "5";
+  } else if (e.key === "6") {
+    res.value += "6";
+  } else if (e.key === "7") {
+    res.value += "7";
+  } else if (e.key === "7") {
+    res.value += "7";
+  } else if (e.key === "8") {
+    res.value += "8";
+  } else if (e.key === "9") {
+    res.value += "9";
+  }
+
+
+  if (e.key === "+") {
+    res.value += "+";
+  } else if (e.key === "-") {
+    res.value += "-";
+  } else if (e.key === "*") {
+    res.value += "*";
+  } else if (e.key === "/") {
+    res.value += "/";
+  }
+
+
+  if (e.key === ".") {
+    res.value += ".";
+  }
+
+
+  if (e.key === "Enter") {
+    cal(result.value);
+  }
+
+
+}
